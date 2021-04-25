400778575115ebd9c5bb14f3c3af971afcfed7ef
Wizzel1 <r_christian@gmx.de>
2021-04-25 21:06:29 +0200

    Add animations: Final Reward, Streak, Button

--- lib/screens/rewardPopupScreen.dart
+++ lib/screens/rewardPopupScreen.dart
@@ -18,42 +18,111 @@ class RewardPopupScreen extends StatefulWidget {
 }
 
 class _RewardPopupScreenState extends State<RewardPopupScreen>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   ScrollController _scrollController;
-  AnimationController _animController;
-  Animation<double> _scaleAnimation;
+  AnimationController _popupAnimController;
+  AnimationController _finalRewardAnimController;
+  Animation<double> _popupScaleAnimation;
+  Animation<double> _popupSequence;
+  List<Animation<Offset>> _slideUpAnimations = [];
+  List<Animation<double>> _fadeInAnimations = [];
   List<Reward> _shuffeledRewardList = [];
   List<Reward> _rewardList = [];
 
-  final double rewardPercentage = 0.5;
+  final double _rewardPercentage = 0.5;
   final ContentController _contentController = Get.find<ContentController>();
 
   @override
   void initState() {
     _scrollController = ScrollController();
-    _animController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 450));
-    _scaleAnimation =
-        CurvedAnimation(parent: _animController, curve: Curves.elasticInOut);
-
+    _initializeAnimations();
     _cloneRewardsIntoShuffeledRewardList();
     _addPercentageBasedEmptyRewards();
     _shuffeledRewardList.shuffle();
     _evaluateRewardVariable(_shuffeledRewardList.last);
-    _animController.forward();
+    _popupAnimController.forward();
 
     super.initState();
 
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      animateRewardList();
+      _animateRewardList();
     });
   }
 
   @override
   void dispose() {
-    _animController.dispose();
+    _popupAnimController.dispose();
+    _finalRewardAnimController.dispose();
     _scrollController.dispose();
     super.dispose();
+  }
+
+  void _initializeAnimations() {
+    _popupAnimController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 450));
+    _finalRewardAnimController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+    _popupScaleAnimation = CurvedAnimation(
+        parent: _popupAnimController, curve: Curves.elasticInOut);
+
+    _popupSequence = TweenSequence([
+      TweenSequenceItem(
+          tween: Tween<double>(begin: 1.0, end: 1.2), weight: 1.0),
+      TweenSequenceItem(
+          tween: Tween<double>(begin: 1.2, end: 1.0), weight: 1.0),
+    ]).animate(
+      CurvedAnimation(
+          parent: _finalRewardAnimController,
+          curve: Interval(
+            0.15,
+            0.55,
+            curve: Curves.ease,
+          )),
+    );
+
+    _slideUpAnimations = [
+      Tween<Offset>(begin: const Offset(0, 1), end: const Offset(0, 0)).animate(
+        CurvedAnimation(
+            parent: _finalRewardAnimController,
+            curve: Interval(
+              0.5,
+              0.75,
+              curve: Curves.ease,
+            )),
+      ),
+      Tween<Offset>(begin: const Offset(0, 1), end: const Offset(0, 0)).animate(
+        CurvedAnimation(
+            parent: _finalRewardAnimController,
+            curve: Interval(
+              0.75,
+              1,
+              curve: Curves.ease,
+            )),
+      ),
+    ];
+
+    _fadeInAnimations = [
+      Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(
+            parent: _finalRewardAnimController,
+            curve: Interval(
+              0.5,
+              0.75,
+              curve: Curves.ease,
+            )),
+      ),
+      Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(
+            parent: _finalRewardAnimController,
+            curve: Interval(
+              0.75,
+              1,
+              curve: Curves.ease,
+            )),
+      ),
+    ];
   }
 
   void _cloneRewardsIntoShuffeledRewardList() {
@@ -96,25 +165,28 @@ class _RewardPopupScreenState extends State<RewardPopupScreen>
 
   void _addPercentageBasedEmptyRewards() {
     double totalRewards = _shuffeledRewardList.length /
-        (widget.isTutorial ? 1.0 : rewardPercentage);
+        (widget.isTutorial ? 1.0 : _rewardPercentage);
     double badRewards = totalRewards - _shuffeledRewardList.length;
 
     for (var i = 0; i < badRewards; i++) {
+      //TODO translate
       Reward _empty = Reward(name: "Keep going!", isSelfRemoving: false);
       _shuffeledRewardList.add(_empty);
     }
   }
 
-  void animateRewardList() {
+  void _animateRewardList() {
     Timer(
       Duration(seconds: 1),
-      () {
-        Future.forEach(_shuffeledRewardList, (reward) async {
-          double offset = (_shuffeledRewardList.indexOf(reward) + 1) * 100.0;
-          await _scrollController.animateTo(offset,
+      () async {
+        await Future.forEach(_shuffeledRewardList, (reward) async {
+          double _offset = (_shuffeledRewardList.indexOf(reward) + 1) * 100.0;
+
+          await _scrollController.animateTo(_offset,
               duration: Duration(milliseconds: 200), curve: Curves.ease);
-        }).then((value) =>
-            print("popup")); //TODO give the chosen reward a popup effect);
+        });
+
+        await _finalRewardAnimController.forward();
       },
     );
   }
@@ -136,7 +208,7 @@ class _RewardPopupScreenState extends State<RewardPopupScreen>
         padding: const EdgeInsets.all(30.0),
         child: Center(
           child: ScaleTransition(
-            scale: _scaleAnimation,
+            scale: _popupScaleAnimation,
             child: Container(
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
@@ -154,66 +226,112 @@ class _RewardPopupScreenState extends State<RewardPopupScreen>
                         itemExtent: 100,
                         childDelegate: ListWheelChildBuilderDelegate(
                           childCount: _shuffeledRewardList.length,
-                          builder: (context, index) => Padding(
-                            padding: const EdgeInsets.fromLTRB(
-                                20.0, 10.0, 20.0, 10.0),
-                            child: Container(
-                              height: 70,
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                color: kLightOrange,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 20.0, vertical: 10),
-                                child: Center(
-                                  child: Text(
-                                    _shuffeledRewardList[index].name,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .button
-                                        .copyWith(
-                                            color: kBackGroundWhite,
-                                            fontSize: 20),
-                                    textAlign: TextAlign.center,
+                          builder: (context, index) => index ==
+                                  (_shuffeledRewardList.length - 1)
+                              ? ScaleTransition(
+                                  scale: _popupSequence,
+                                  child: Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        20.0, 10.0, 20.0, 10.0),
+                                    child: Container(
+                                      height: 70,
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                        color: kLightOrange,
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 20.0, vertical: 10),
+                                        child: Center(
+                                          child: Text(
+                                            _shuffeledRewardList[index].name,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .button
+                                                .copyWith(
+                                                    color: kBackGroundWhite,
+                                                    fontSize: 20),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              : Padding(
+                                  padding: const EdgeInsets.fromLTRB(
+                                      20.0, 10.0, 20.0, 10.0),
+                                  child: Container(
+                                    height: 70,
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      color: kLightOrange,
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 20.0, vertical: 10),
+                                      child: Center(
+                                        child: Text(
+                                          _shuffeledRewardList[index].name,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .button
+                                              .copyWith(
+                                                  color: kBackGroundWhite,
+                                                  fontSize: 20),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                          ),
                         ),
                       ),
                     ),
                     const SizedBox(height: 30),
-                    Text(
-                      "You are on a ${widget.habit.streak} streak!",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline6
-                          .copyWith(color: kDeepOrange),
+                    SlideTransition(
+                      position: _slideUpAnimations[0],
+                      child: FadeTransition(
+                        opacity: _fadeInAnimations[0],
+                        //TODO translate
+                        child: Text(
+                          "You are on a ${widget.habit.streak}x streak!",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6
+                              .copyWith(color: kDeepOrange),
+                        ),
+                      ),
                     ),
                     const SizedBox(height: 30),
-                    Container(
-                      height: 60,
-                      width: 60,
-                      child: MaterialButton(
-                        padding: const EdgeInsets.all(0),
-                        elevation: 0,
-                        color: Colors.green[400],
-                        child: const Center(
-                          child: Icon(
-                            Icons.check,
-                            color: Colors.white,
-                            size: 30,
+                    SlideTransition(
+                      position: _slideUpAnimations[1],
+                      child: FadeTransition(
+                        opacity: _fadeInAnimations[1],
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          child: MaterialButton(
+                            padding: const EdgeInsets.all(0),
+                            elevation: 0,
+                            color: Colors.green[400],
+                            child: const Center(
+                              child: Icon(
+                                Icons.check,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                            onPressed: () {
+                              _checkIfRewardIsRemoving();
+                              Navigator.of(context).pop();
+                            },
                           ),
                         ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8)),
-                        onPressed: () {
-                          _checkIfRewardIsRemoving();
-                          Navigator.of(context).pop();
-                        },
                       ),
                     )
                   ],
