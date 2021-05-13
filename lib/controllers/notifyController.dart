b57f6915465998a6ad9d40fae93600cfd3e5f2f2
Wizzel1 <r_christian@gmx.de>
2021-05-13 23:47:43 +0200

    fix: Change launcher icon reference

--- lib/controllers/notifyController.dart
+++ lib/controllers/notifyController.dart
@@ -29,7 +29,7 @@ class NotifyController extends GetxController {
   Future<bool> initializeNotificationPlugin() async {
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('app_icon');
+        AndroidInitializationSettings("@mipmap/launcher_icon");
     final IOSInitializationSettings initializationSettingsIOS =
         IOSInitializationSettings(
             onDidReceiveLocalNotification: onDidReceiveLocalNotification);
