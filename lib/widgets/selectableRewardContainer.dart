6ebedc98294b510197ecd0281b2c14254ae9c1fc
Wizzel1 <r_christian@gmx.de>
2021-05-16 20:23:25 +0200

    Unwrap rewardcontainer from neumorphic

--- lib/widgets/selectableRewardContainer.dart
+++ lib/widgets/selectableRewardContainer.dart
@@ -24,44 +24,44 @@ class SelectableRewardContainer extends StatefulWidget {
 class _SelectableRewardContainerState extends State<SelectableRewardContainer> {
   @override
   Widget build(BuildContext context) {
-    return NeumorphPressSwitch(
-      onPressed: () {
-        widget.onTap();
-      },
-      height: 70,
-      width: double.infinity,
-      style: widget.isSelectedReward
-          ? kActiveNeumorphStyle
-          : kInactiveNeumorphStyle,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Material(
-                  type: MaterialType.transparency,
-                  child: Text(
-                    widget.reward.name,
-                    style: Theme.of(context).textTheme.bodyText1.copyWith(
-                          color: widget.isSelectedReward
-                              ? kBackGroundWhite
-                              : kDeepOrange,
-                        ),
+    return GestureDetector(
+      onTap: widget.onTap,
+      child: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: widget.isSelectedReward ? kDeepOrange : kBackGroundWhite,
+        ),
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Material(
+                    type: MaterialType.transparency,
+                    child: Text(
+                      widget.reward.name,
+                      style: Theme.of(context).textTheme.bodyText1.copyWith(
+                            color: widget.isSelectedReward
+                                ? kBackGroundWhite
+                                : kDeepOrange,
+                          ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Icon(
-              widget.reward.isSelfRemoving
-                  ? FontAwesomeIcons.ban
-                  : FontAwesomeIcons.redoAlt,
-              color: widget.isSelectedReward ? kBackGroundWhite : kDeepOrange,
-            )
-          ],
+                ],
+              ),
+              Icon(
+                widget.reward.isSelfRemoving
+                    ? FontAwesomeIcons.ban
+                    : FontAwesomeIcons.redoAlt,
+                color: widget.isSelectedReward ? kBackGroundWhite : kDeepOrange,
+              )
+            ],
+          ),
         ),
       ),
     );
